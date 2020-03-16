7050d0bf27f8913f7fd627781fc67c7f3e2cca13
Filipe Rocha <fsrbillie@gmail.com>
2020-03-16 16:25:55 -0300

    todo finalizado

--- todo-app/frontend/src/main/app.jsx
+++ todo-app/frontend/src/main/app.jsx
@@ -1,6 +1,6 @@
 import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
-/*import '../template/custom.css'*/
+import '../template/custom.css'
 
 import React from 'react'
 import Menu from '../template/menu'
@@ -11,6 +11,6 @@ import Routes from './routes'
 export default props => (
     <div className='container'>
       <Menu />
-      <Routes />        
+      <Routes />
     </div>
 )
